9c2edefa812374109d2e01b559083e8124e61ca5
Angela Shu <angela.shu@mongodb.com>
2024-06-25 14:16:50 -0400

    make write data page

---
+++ source/includes/usage-examples/sample-app-intro.rst
@@ -0,0 +1,18 @@
+.. TODO: change link from quick start primer to subscriber implementation page
+
+You can use the following sample application to test the code examples on this
+page. To use the sample application, perform the following steps:
+
+1. Ensure you have {+driver-short+} installed.
+#. Create a new Java project in your IDE.
+#. Create a package called ``helpers`` within the existing ``java`` package in
+   your project.
+#. Create a new Java file in the ``helpers`` package
+   named ``SubscriberHelpers``. Implement the ``Subscriber`` implementations, which are
+   described in the :ref:`Quick Start Primer <javars-primer>`, in the
+   ``SubscriberHelpers`` file.
+#. Copy the following code and paste it into a new ``.java`` file named ``updateDocument``.
+#. Copy a code example from this page and paste it on the specified lines in the
+   file.
+
+
---
+++ source/includes/usage-examples/sample-write-application.java
@@ -0,0 +1,60 @@
+import com.mongodb.*;
+import com.mongodb.client.model.DeleteOptions;
+import com.mongodb.client.model.InsertManyOptions;
+import com.mongodb.client.model.InsertOneOptions;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertManyResult;
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.reactivestreams.client.MongoCollection;
+
+import helpers.SubscriberHelpers;
+import helpers.SubscriberHelpers.ObservableSubscriber;
+import helpers.SubscriberHelpers.PrintDocumentSubscriber;
+import org.bson.Document;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+import static com.mongodb.client.model.Updates.set;
+import static com.mongodb.client.model.Updates.combine;
+
+public class updateDocument {
+    public static void main(String[] args) {
+        // Replace the placeholder with your Atlas connection string
+        String uri = "<connection string>";
+
+        // Construct a ServerApi instance using the ServerApi.builder() method
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings))
+        {
+            MongoDatabase sample_restaurants = mongoClient.getDatabase("sample_restaurants");
+            MongoCollection<Document> restaurants = sample_restaurants.getCollection("restaurants");
+
+            try {
+                // Start example code here
+
+                // End example code here
+            } catch (MongoException me) {
+                System.err.println(me);
+            }
+        }
+    }
+}
