75670f17c58734c2d00f8ac6af97ecfeaa837337
Angela Shu <angela.shu@mongodb.com>
2024-07-15 11:54:39 -0400

    avoid scroll

--- source/includes/usage-examples/write-code-examples.java
+++ source/includes/usage-examples/write-code-examples.java
@@ -2,7 +2,8 @@
 Document document = new Document("<field name>", "<value>");
 Mono<InsertOneResult> result = Mono.from(collection.insertOne(document));
 
-System.out.printf("Inserted 1 document with ID %s%n.", result.block().getInsertedId());
+System.out.printf("Inserted 1 document with ID %s%n.", 
+      result.block().getInsertedId());
 # end-insert-one
 
 # start-insert-multiple
@@ -13,7 +14,8 @@ List<Document> documents = Arrays.asList(doc1, doc2);
 
 Mono<InsertManyResult> result = Mono.from(collection.insertMany(documents));
 
-System.out.printf("Inserted documents with IDs %s%n.", result.block().getInsertedIds());
+System.out.printf("Inserted documents with IDs %s%n.",
+      result.block().getInsertedIds());
 # end-insert-multiple
 
 # start-update-one
@@ -21,7 +23,8 @@ Mono<UpdateResult> result = Mono.from(collection.updateOne(
       eq("<field name>", "<value>"),
       set("<field name>", "<new value>")));
 
-System.out.printf("Inserted %s document with ID %s%n.", result.block().getModifiedCount(), result.block().getUpsertedId());
+System.out.printf("Inserted %s document with ID %s%n.",
+      result.block().getModifiedCount(), result.block().getUpsertedId());
 # end-update-one
 
 # start-update-multiple
@@ -29,7 +32,8 @@ Mono<UpdateResult> result = Mono.from(collection.updateMany(
       eq("<field name>", "<value>"),
       set("<field name>", "<new value>")));
 
-System.out.printf("Inserted %s documents with ID %s%n.", result.block().getModifiedCount(), result.block().getUpsertedId());
+System.out.printf("Inserted %s documents with ID %s%n.",
+      result.block().getModifiedCount(), result.block().getUpsertedId());
 # end-update-multiple
 
 # start-replace-one
@@ -38,17 +42,20 @@ Mono<UpdateResult> result = Mono.from(collection.replaceOne(
       new Document().append("<field name>", "<new value>")
       .append("<new field name>", "<new value>")));
 
-System.out.printf("Replaced %s document with ID %s%n.", result.block().getModifiedCount(), result.block().getUpsertedId());
+System.out.printf("Replaced %s document with ID %s%n.",
+      result.block().getModifiedCount(), result.block().getUpsertedId());
 # end-replace-one
 
 # start-delete-one
-Mono<DeleteResult> result = Mono.from(collection.deleteOne(eq("<field name>", "<value>")));
+Mono<DeleteResult> result = Mono.from(collection.deleteOne(
+      eq("<field name>", "<value>")));
 
 System.out.printf("Deleted %s document.", result.block().getDeletedCount());
 # end-delete-one
 
 # start-delete-multiple
-Mono<DeleteResult> result = Mono.from(collection.deleteMany(eq("<field name>", "<value>")));
+Mono<DeleteResult> result = Mono.from(collection.deleteMany(
+      eq("<field name>", "<value>")));
 
 System.out.printf("Deleted %s documents.", result.block().getDeletedCount());
 # end-delete-multiple
@@ -58,10 +65,13 @@ Mono<BulkWriteResult> result = Mono.from(collection.bulkWrite(
     Arrays.asList(new InsertOneModel<>(new Document("<field name>", "<value>")),
         new InsertOneModel<>(new Document("<field name>", "<value>")),
         new UpdateOneModel<>(new Document("<field name>", "<value>"),
-                             new Document("$set", new Document("<field name>", "<new value>"))),
+                             new Document("$set",
+                             new Document("<field name>", "<new value>"))),
         new DeleteOneModel<>(new Document("<field name>", "<value>")),
         new ReplaceOneModel<>(new Document("<field name>", "<value>"),
-                              new Document("<field name>", "<new value>").append("<new field name>", "<new value>")))));
+                              new Document("<field name>", "<new value>")
+                              .append("<new field name>", "<new value>")))));
 
-System.out.printf("Modified %s documents and deleted %s%n documents.", result.block().getModifiedCount(), result.block().getDeletedCount);
+System.out.printf("Modified %s documents and deleted %s%n documents."
+      result.block().getModifiedCount(), result.block().getDeletedCount);
 # end-bulk-write
