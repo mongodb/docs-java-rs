226bbb23130f3689c61346a2277bcba35ee26cc6
Angela Shu <angela.shu@mongodb.com>
2024-07-19 16:42:22 -0400

    remote imports

--- source/includes/project.java
+++ source/includes/project.java
@@ -2,15 +2,12 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
-import com.mongodb.client.result.InsertManyResult;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.FindPublisher;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 import org.bson.BsonDocument;
-import org.bson.BsonInt64;
-
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -20,11 +17,8 @@ import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import static com.mongodb.client.model.Aggregates.limit;
-import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Indexes.ascending;
+import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Projections.*;
 
 class QueryDatabase {
