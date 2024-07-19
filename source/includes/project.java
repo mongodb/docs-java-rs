7adf92d5f683982b72dd2611699c8130aa6265ea
Angela Shu <angela.shu@mongodb.com>
2024-07-19 17:31:08 -0400

    fix spacing

--- source/includes/project.java
+++ source/includes/project.java
@@ -44,41 +44,41 @@ class QueryDatabase {
            MongoDatabase database = mongoClient.getDatabase("sample_restaurants");
            MongoCollection<Document> restaurants = database.getCollection("restaurants");
 
-            //  start-project-include
-            FindPublisher<Document> findProjectionPublisher = restaurants.find(
-                            eq("name", "Emerald Pub"))
-                    .projection(fields(include("name", "cuisine", "borough")));
-            List<Document> findResults = Flux.from(findProjectionPublisher)
-                    .collectList().block();
+//  start-project-include
+FindPublisher<Document> findProjectionPublisher = restaurants.find(
+                eq("name", "Emerald Pub"))
+        .projection(fields(include("name", "cuisine", "borough")));
+List<Document> findResults = Flux.from(findProjectionPublisher)
+        .collectList().block();
 
-            for (Document document : findResults) {
-                System.out.println(document);
-            }
-            //  end-project-include
+for (Document document : findResults) {
+    System.out.println(document);
+}
+//  end-project-include
 
-            //  start-project-include-without-id
-            FindPublisher<Document> findProjectionPublisher = restaurants.find(
-                            eq("name", "Emerald Pub"))
-                    .projection(fields(include("name", "cuisine", "borough"), excludeId()));
-            List<Document> findResults = Flux.from(findProjectionPublisher)
-                    .collectList().block();
+//  start-project-include-without-id
+FindPublisher<Document> findProjectionPublisher = restaurants.find(
+                eq("name", "Emerald Pub"))
+        .projection(fields(include("name", "cuisine", "borough"), excludeId()));
+List<Document> findResults = Flux.from(findProjectionPublisher)
+        .collectList().block();
 
-            for (Document document : findResults) {
-                System.out.println(document);
-            }
-            //  end-project-include-without-id
+for (Document document : findResults) {
+    System.out.println(document);
+}
+//  end-project-include-without-id
 
-            //  start-project-exclude
-            FindPublisher<Document> findProjectionPublisher = restaurants.find(
-                            eq("name", "Emerald Pub"))
-                    .projection(fields(exclude("grades", "address")));
-            List<Document> findResults = Flux.from(findProjectionPublisher)
-                    .collectList().block();
+//  start-project-exclude
+FindPublisher<Document> findProjectionPublisher = restaurants.find(
+                eq("name", "Emerald Pub"))
+        .projection(fields(exclude("grades", "address")));
+List<Document> findResults = Flux.from(findProjectionPublisher)
+        .collectList().block();
 
-            for (Document document : findResults) {
-                System.out.println(document);
-            }
-            //  end-project-exclude
+for (Document document : findResults) {
+    System.out.println(document);
+}
+//  end-project-exclude
 
         }
     }
