b0e13fcca99fab8e5c2c5267af6a9b46bbec00a4
Angela Shu <angela.shu@mongodb.com>
2024-07-23 11:43:11 -0400

    change code based on rl feedback on diff pr

--- source/includes/project.java
+++ source/includes/project.java
@@ -48,36 +48,30 @@ class QueryDatabase {
         FindPublisher<Document> findProjectionPublisher = restaurants.find(
                         eq("name", "Emerald Pub"))
                 .projection(fields(include("name", "cuisine", "borough")));
-        List<Document> findResults = Flux.from(findProjectionPublisher)
-                .collectList().block();
 
-        for (Document document : findResults) {
-            System.out.println(document);
-        }
+        Document findResults = Flux.from(findProjectionPublisher)
+                 .doOnNext(System.out::println)
+                 .blockLast();
         //  end-project-include
 
         //  start-project-include-without-id
         FindPublisher<Document> findProjectionPublisher = restaurants.find(
                         eq("name", "Emerald Pub"))
                 .projection(fields(include("name", "cuisine", "borough"), excludeId()));
-        List<Document> findResults = Flux.from(findProjectionPublisher)
-                .collectList().block();
 
-        for (Document document : findResults) {
-            System.out.println(document);
-        }
+        Document findResults = Flux.from(findProjectionPublisher)
+                 .doOnNext(System.out::println)
+                 .blockLast();
         //  end-project-include-without-id
 
         //  start-project-exclude
         FindPublisher<Document> findProjectionPublisher = restaurants.find(
                         eq("name", "Emerald Pub"))
                 .projection(fields(exclude("grades", "address")));
-        List<Document> findResults = Flux.from(findProjectionPublisher)
-                .collectList().block();
-
-        for (Document document : findResults) {
-            System.out.println(document);
-        }
+                
+        Document findResults = Flux.from(findProjectionPublisher)
+                 .doOnNext(System.out::println)
+                 .blockLast();
         //  end-project-exclude
 
         }
