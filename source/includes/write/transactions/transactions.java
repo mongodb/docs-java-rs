cb0cf8025a712e5aed991f5978b2ff3519643920
Jordan Smith <jordan.smith@mongodb.com>
2024-08-15 10:22:30 -0700

    first draft transactions page

---
+++ source/includes/write/transactions/transactions.java
@@ -0,0 +1,53 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import org.bson.Document;
+import reactor.core.publisher.Mono;
+
+public class TransactionExample {
+    public static void main(String[] args) {
+        String uri = "<connection string>";
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .build();
+
+        try {
+            MongoClient mongoClient = MongoClients.create(settings);
+
+            MongoDatabase restaurantsDatabase = mongoClient.getDatabase("sample_restaurants");
+            MongoCollection<Document> restaurants = restaurantsDatabase.getCollection("restaurants");
+            MongoDatabase moviesDatabase = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> movies = moviesDatabase.getCollection("movies");
+
+            Mono.from(mongoClient.startSession())
+                    .flatMap(session -> {
+                        // Begins the transaction
+                        session.startTransaction();
+
+                        // Inserts documents in the given order
+                        return Mono
+                                .from(restaurants.insertOne(session,
+                                        new Document("name", "Reactive Streams Pizza").append("cuisine", "Pizza")))
+                                .then(Mono.from(movies.insertOne(session,
+                                        new Document("title", "Java: Into the Streams").append("type", "Movie"))))
+                                // Commits the transaction
+                                .flatMap(result -> Mono.from(session.commitTransaction())
+                                        .thenReturn(result))
+                                .onErrorResume(error -> Mono.from(session.abortTransaction()).then(Mono.error(error)))
+                                .doFinally(signalType -> session.close());
+                    })
+                    // Closes the client after the transaction completes
+                    .doFinally(signalType -> mongoClient.close())
+                    // Prints the results of the transaction
+                    .subscribe(
+                            result -> System.out.println("Transaction succeeded"),
+                            error -> System.err.println("Transaction failed: " + error));
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
