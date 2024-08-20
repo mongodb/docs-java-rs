bb186f113aa4dcb275add2a2235cfc1d91333e58
Jordan Smith <jordan.smith@mongodb.com>
2024-08-20 08:16:00 -0700

    edits

--- source/includes/write/run-command.java
+++ source/includes/write/run-command.java
@@ -1,7 +1,5 @@
-// start-run-command
 Document command = new Document("ping", 1);
 
 Publisher<Document> commandPublisher = database.runCommand(command);
 Document result = Mono.from(commandPublisher).block();
 System.out.println(result);
-// end-run-command
