f92f60f502903c0d1af7a928529f7adc1d4b2333
Michael Morisi <michael.morisi@mongodb.com>
2024-08-20 10:30:34 -0400

    DOCSP-42298: Stable API

---
+++ source/includes/connect/stable-api.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+
+import com.mongodb.reactivestreams.client.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // start-enable-stable-api
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        // Replace the placeholder with your Atlas connection string
+        String uri = "<connection string URI>";
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            // Perform client operations here
+        }
+        // end-enable-stable-api
+
+        // start-stable-api-options
+        ServerApi serverApi = ServerApi.builder()
+            .version(ServerApiVersion.V1)
+            .strict(true)
+            .deprecationErrors(true)
+            .build();
+        // end-stable-api-options
+    }
+}
