430b62f39084beb0c657a55722b0347f078681ca
Jordan Smith <jordan.smith@mongodb.com>
2024-08-26 07:42:18 -0700

    remove import

--- source/includes/connect/network-compression.java
+++ source/includes/connect/network-compression.java
@@ -1,19 +1,16 @@
 // start-specify-connection-string
-
-import java.util.Arrays;
-
 ConnectionString connectionString = new ConnectionString(
-                "mongodb+srv://<db_username>:<db_password>@<cluster-url>/?compressors=snappy,zlib,zstd");
+        "mongodb+srv://<db_username>:<db_password>@<cluster-url>/?compressors=snappy,zlib,zstd");
 
 MongoClient client = MongoClients.create(connectionString);
 // end-specify-connection-string
 
 // start-specify-uri
 MongoClientSettings settings = MongoClientSettings.builder()
-                .compressorList(Arrays.asList(MongoCompressor.createSnappyCompressor(),
-                                MongoCompressor.createZlibCompressor(),
-                                MongoCompressor.createZstdCompressor()))
-                .build();
+        .compressorList(Arrays.asList(MongoCompressor.createSnappyCompressor(),
+                MongoCompressor.createZlibCompressor(),
+                MongoCompressor.createZstdCompressor()))
+        .build();
 
 MongoClient client = MongoClients.create(settings);
 // end-specify-uri
