f89382b03d9db7fb54abc47344981a8dd4c08306
Michael Morisi <michael.morisi@mongodb.com>
2024-08-27 15:56:10 -0400

    fix

--- source/includes/usage-examples/sample-index-application.java
+++ source/includes/usage-examples/sample-index-application.java
@@ -13,7 +13,6 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-
 public class IndexOperations {
     public static void main(String[] args) {
         // Replace the placeholder with your Atlas connection string
