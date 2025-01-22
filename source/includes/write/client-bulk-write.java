92339bb404703324d92acfcf3aca79e69bcaa232
rustagir <rea.rustagi@mongodb.com>
2025-01-22 16:24:50 -0500

    wip

--- source/includes/write/client-bulk-write.java
+++ source/includes/write/client-bulk-write.java
@@ -24,39 +24,32 @@ public class QuickStart {
 
         // Create a new client and connect to the server
         try (MongoClient mongoClient = MongoClients.create(settings)) {
-//            List<ClientNamespacedWriteModel> bulkOperations = new ArrayList<>();
 
             // start-insert-models
-            MongoNamespace peopleNamespace = new MongoNamespace("db", "people");
-            MongoNamespace thingsNamespace = new MongoNamespace("db", "things");
-
-            ClientNamespacedInsertOneModel insertDocument1 = ClientNamespacedWriteModel
+            ClientNamespacedInsertOneModel personToInsert = ClientNamespacedWriteModel
                     .insertOne(
-                            peopleNamespace,
+                            new MongoNamespace("db", "people"),
                             new Document("name", "Julia Smith")
             );
 
-            ClientNamespacedInsertOneModel insertDocument2 = ClientNamespacedWriteModel
+            ClientNamespacedInsertOneModel thingToInsert = ClientNamespacedWriteModel
                     .insertOne(
-                            thingsNamespace,
+                            new MongoNamespace("db", "things"),
                             new Document("object", "washing machine")
             );
             // end-insert-models
 
             // start-replace-models
-            MongoNamespace peopleNamespace = new MongoNamespace("db", "people");
-            MongoNamespace thingsNamespace = new MongoNamespace("db", "things");
-
-            ClientNamespacedReplaceOneModel replace_person = ClientNamespacedWriteModel
+            ClientNamespacedReplaceOneModel personReplacement = ClientNamespacedWriteModel
                     .replaceOne(
-                            peopleNamespace,
+                            new MongoNamespace("db", "people"),
                             Filters.eq("_id", 1),
                             new Document("name", "Frederic Hilbert")
                     );
 
-            ClientNamespacedReplaceOneModel replace_thing = ClientNamespacedWriteModel
+            ClientNamespacedReplaceOneModel thingReplacement = ClientNamespacedWriteModel
                     .replaceOne(
-                            thingsNamespace,
+                            new MongoNamespace("db", "things"),
                             Filters.eq("_id", 1),
                             new Document("object", "potato")
                     );
@@ -79,10 +72,14 @@ public class QuickStart {
                                     new Document("object", "potato")
                             )
             );
-            Publisher<ClientBulkWriteResult> bulkWritePublisher = mongoClient.bulkWrite(
-                    bulkOperations);
+            
+            Publisher<ClientBulkWriteResult> bulkWritePublisher = mongoClient
+                    .bulkWrite(bulkOperations);
 
-            ClientBulkWriteResult clientBulkResult = Mono.from(bulkWritePublisher).block();
+            ClientBulkWriteResult clientBulkResult = Mono
+                    .from(bulkWritePublisher)
+                    .block();
+            
             System.out.println(clientBulkResult.toString());
             // end-perform
 
@@ -93,32 +90,24 @@ public class QuickStart {
                     .clientBulkWriteOptions()
                     .ordered(false);
 
-            List<ClientNamespacedWriteModel> bulkOperations = new ArrayList<>();
-
-            bulkOperations.add(
+            List<ClientNamespacedWriteModel> bulkOperations = Arrays.asList(
                     ClientNamespacedWriteModel.insertOne(
                             namespace,
                             new Document("_id", 1).append("name", "Rudra Suraj")
-                    )
-            );
-
-            // Causes a duplicate key error
-            bulkOperations.add(
+                    ),
+                    // Causes a duplicate key error
                     ClientNamespacedWriteModel.insertOne(
                             namespace,
                             new Document("_id", 1).append("name", "Mario Bianchi")
-                    )
-            );
-
-            bulkOperations.add(
+                    ),
                     ClientNamespacedWriteModel.insertOne(
                             namespace,
                             new Document("name", "Wendy Zhang")
                     )
             );
 
-            Publisher<ClientBulkWriteResult> bulkWritePublisher = mongoClient.bulkWrite(
-                    bulkOperations, options);
+            Publisher<ClientBulkWriteResult> bulkWritePublisher = mongoClient
+                    .bulkWrite(bulkOperations, options);
             // end-options
         }
     }
